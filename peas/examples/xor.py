188184735573d7a0f8b36d2a4e47c59f4fe7058f
Nao Tokui <tokui (at) qosmo.jp>
2015-09-06 18:10:52 +0900

    Fixed: warning - the function must be able to output -1

--- peas/examples/xor.py
+++ peas/examples/xor.py
@@ -14,7 +14,7 @@ from peas.tasks.xor import XORTask
 # instance each time it is called)
 genotype = lambda: NEATGenotype(inputs=2, 
                                 weight_range=(-3., 3.), 
-                                types=['sigmoid2'])
+                                types=['tanh'])
 
 # Create a population
 pop = NEATPopulation(genotype, popsize=150)
